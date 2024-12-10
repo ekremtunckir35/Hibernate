b962b9142b6cca850d0a1e96f950ae77a93060f3
EkremTnckr <ekremtunckir@gmail.com>
2024-12-10 14:02:56 +0300

    hibernate CRUD islemleri,HQL

---
+++ HibernateInstruction/src/main/java/com/tpe/hb01/basicannotations/Employee.java
@@ -0,0 +1,64 @@
+package com.tpe.hb01.basicannotations;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Transient;
+
+@Entity
+public class Employee {
+
+    @Id
+    private Long id;
+
+
+    private String name;
+
+    private Double salary;//50$
+
+    @Transient // DB de bu field de karsilik bir sutunu olusturmasini engeller
+    private Double bonus;//50 puan bosu varsa maasi 50$ artiracagim
+    //bonusun tabloda yer almasina gerek yok
+
+
+   // @Lob  // Large Object: bu sütunda büyük boyutlu datalar saklanır:resim,video,ses...
+   // private byte[] image;
+
+
+    //getter setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(Double bonus) {
+        this.bonus = bonus;
+    }
+
+
+}
--- HibernateInstruction/src/main/java/com/tpe/hb01/basicannotations/RunnerSave01.java
+++ HibernateInstruction/src/main/java/com/tpe/hb01/basicannotations/RunnerSave01.java
@@ -2,17 +2,69 @@ package com.tpe.hb01.basicannotations;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+/*
+Configuration, Hibernate'in başlangıç aşamasında kullanılır
+ve Hibernate'in çalışması için gerekli ayarların (örneğin, veritabanı bağlantısı)
+ yapılmasını sağlar. Hibernate ile çalışırken tipik olarak bir Configuration
+  nesnesi oluşturulur, ardından bu nesne kullanılarak bir SessionFactory oluşturulur.
+
+SessionFactory, Hibernate'in temel bileşenlerinden biridir
+Veritabanı işlemleri gerçekleştirmek için Session nesneleri üretir.
+Uygulama boyunca genellikle bir kez oluşturulur ve tüm uygulama tarafından paylaşılır.
+
+Session, Hibernate ile veritabanı arasında bağlantıyı sağlar.
+Veritabanı üzerinde Create, Read, Update, Delete (CRUD) işlemlerini gerçekleştirir.
+Her işlem için yeni bir Session oluşturulması önerilir.
+ */
+
+
 
 public class RunnerSave01 {
     public static void main(String[] args) {
 
+        Student student1 = new Student();
+        student1.setId(1001);
+        student1.setName("Jack Sparrow");
+        student1.setGrade(99);
+
+        Student student2 = new Student();
+        student2.setId(1002);
+        student2.setName("Harry Potter");
+        student2.setGrade(98);
+
+        Student student3 = new Student();
+        student3.setId(1003);
+        student3.setName("Sharlock Holmes");
+        student3.setGrade(98);
+
+
+
         Configuration config=new Configuration().configure("hibernate.cfg.xml").
-                addAnnotatedClass(Student.class);
+                addAnnotatedClass(Student.class).addAnnotatedClass(Employee.class);
 
         SessionFactory sessionFactory =config.buildSessionFactory();
 
         Session session =sessionFactory.openSession();
+
+
+        //hibernatede default olarak auto-commit:false
+        //db de işlemlerin kalıcı olması için transaction başlatılıp onaylanması gerekir
+
+
+        Transaction transaction =session.beginTransaction();//transaction(db de atomik işlem birimi) başlatıldı.
+
+        //student1 i tabloya ekleyelim INSERT INTO T_student VALUES(...)
+
+        session.save(student1);
+        session.save(student2);
+        session.save(student3);
+
+        transaction.commit();//transaction onaylar ve sonlandirir
+        session.close();
+        sessionFactory.close(); //uygulama tamamlandigi zaman sessionFactory kapatilir.
+
 
     }
 }
